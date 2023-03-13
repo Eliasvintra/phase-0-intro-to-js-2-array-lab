9e6e09727e7a12e2f49be846ada2056b148e8f0c
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-13 12:01:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];// Write your solution here!
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+function appendCat(name) {
+    let cat = cats.slice();
+    cat.push("Broom");
+
+    return cat;
+}
+function prependCat(name) {
+    let cat = cat.slice();
+    cat.unshift("Arnold");
+    
+    return cat;
+}
+function removeLastCat() {
+    let cat = cat.slice();
+    cat.pop();
+    
+    return cat;
+}
+function prependCat(name) {
+    let cat = cats.slice();
+    cat.unshift("Arnold");
+
+    return cat;
+}
+function removeLastCat() {
+    let cat = cats.slice();
+    cat.pop();
+    return cat;
+}
+function removeFirstCat() {
+    let cat = cats.slice();
+    cat.shift();
+    return cat;
+}
